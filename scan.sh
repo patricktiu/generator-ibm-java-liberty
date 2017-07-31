20c8b053a482857b3597e24cc5508b683caae50c
MarkNSweep <MarkNSweep@github.com>
2017-07-31 10:50:26 +0100

    fix(infrastructure): update ossc scan with new tool

--- scan.sh
+++ scan.sh
@@ -6,9 +6,9 @@ cd ./production
 git clone git@github.ibm.com:arf/java-codegen-liberty.git
 cd ./java-codegen-liberty/generator-liberty
 npm install --only=production
-npm install @ibm/osc-cli -g
+npm install @wicked/cli -g
 echo "Running scan"
-osc-cli -s . -o -u travisci@travis.ibm.com
+wicked-cli -s . -u travisci@travis.ibm.com
 cd $WORKING_DIR
 echo "Copying scan results file to ../docs"
-cp ./production/java-codegen-liberty/generator-liberty/generator-liberty_scanresults* ../docs/scan
+cp ./production/java-codegen-liberty/generator-liberty/generator-liberty_scan-results/* ../docs/scan
